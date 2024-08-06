f01696b0bb6d70fe08ab88aeb6cfdbe27c7c14b6
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2024-08-06 14:06:51 +0200

    update css

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
 
         <div className='container font-monospace d-flex justify-content-center'>
           {/* alpha 0.5 */}
-          <div className="card text-white m-5 p-3 rounded-3"
+          <div className="card text-white m-3 p-3 rounded-3"
             style={{ backgroundColor: 'rgba(1, 1, 1, 0.5)', width: "28rem" }}>
             <div className="card-header"><h1>Jesús Oroya</h1></div>
             <div className="card-body">
@@ -50,7 +50,7 @@ function App() {
                 Physicist and simulation developer.
               </h5>
               {/* view more */}
-              <div className="d-grid gap-2 d-md-flex justify-content-md-center m-4">
+              <div className="d-grid gap-2 d-md-flex justify-content-md-center ">
                 <button className="btn btn-primary"
                   onClick={() => setPage('about')}>About me</button>
               </div>
